6c0ca93e1a0bf0c7fb73484f31903b1ca7b6cf58
Ruturaj Dixit <rajdixitrd7272@gmail.com>
2022-03-25 11:28:37 +0530

    added mul.java

---
+++ mul.java
@@ -0,0 +1,13 @@
+public class mul {
+  
+
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=4;
+        int x=a*b;
+        System.out.println("This is the multiplication of,"+a+"and"+b+x);
+    }
+}
+    
+
