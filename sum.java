7ca8c28c85503f125fadad4aa7a8a6ea7e4b63ee
Ruturaj Dixit <rajdixitrd7272@gmail.com>
2022-03-25 11:02:46 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class sum{
+    public static void main(String args[])
+    {
+        int a=2;
+        int b=4;
+        int x=a+b;
+        System.out.println(x);
+    }
+}
